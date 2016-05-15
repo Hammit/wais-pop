dd2e5874eea262102d28eb33d0aa671048c4b09a
Byron Hammond <byron_hammond@yahoo.com.au>
2016-05-15 21:43:03 +0800

    Restructured bin scripts

--- bin/Nonin/test.py
+++ bin/Nonin/test.py
@@ -4,5 +4,12 @@ from Nonin import *
 device = Nonin3150.get_device()
 nonin = Nonin3150(device)
 
+import time
+time.sleep(1)
+
+import pdb
 sessions = nonin.read_sessions()
+pdb.set_trace()
+for session in sessions[0]: print session
+print "Finished!"
 
