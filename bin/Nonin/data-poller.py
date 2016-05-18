a95950f5efba8297eaf093d9acf54566aa20404e
Byron <byron_hammond@yahoo.com.au>
2016-05-18 21:23:39 +0800

    Added script to periodically poll the WristOx, saving memory to a csv file and then clearing memory

---
+++ bin/Nonin/data-poller.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+
+import time
+import datetime
+from Nonin import *
+
+def csv_filename(device_id=0):
+    dt = datetime.datetime.now()
+    dt_str = dt.strftime('%Y-%m-%dT%H:%M:%S')
+    filename = '/tmp/wristox-sessions.%d.%s.csv' % (device_id, dt_str)
+    return filename
+
+POLL_INTERVAL = 45 # seconds
+
+device = Nonin3150.get_device()
+nonin = Nonin3150(device)
+
+# time.sleep(5) # wait for the device to finish changing modes if necessary
+
+while True:
+    # Read, Save and Clear session data from the device
+    print "Reading data..."
+    try:
+        sessions = nonin.read_sessions()
+        if sessions is not None:
+            exporter = Exporter(sessions)
+            filename = csv_filename(device_id=2)
+            print "Saving session data to %s" % csv_filename
+            exporter.export(format='csv', filename=filename)
+
+            print "Clearing device memory"
+            nonin.clear_sessions()
+
+            print "Waiting for %d seconds" % POLL_INTERVAL
+            time.sleep(POLL_INTERVAL)
+    except Exception:
+        print "Exception raised, probably an ACK required"
+
+#import pdb
+#pdb.set_trace()
+for session in sessions[0]: print session
+print "Finished!"
+
