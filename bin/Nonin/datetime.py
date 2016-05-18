4a2ed8e4316e3c6f3f8e36bcd8a1bf8e4a729bff
Byron <byron_hammond@yahoo.com.au>
2016-05-18 21:46:39 +0800

    datetime.py now gets and sets the datetime

--- bin/Nonin/datetime.py
+++ bin/Nonin/datetime.py
@@ -4,7 +4,7 @@ from Nonin import *
 device = Nonin3150.get_device()
 nonin = Nonin3150(device)
 
-nonin.enable_logging()
+# nonin.enable_logging()
 
 # Set time on Nonin from laptop, using GMT timezone
 current_time = nonin.get_current_time()
@@ -12,11 +12,11 @@ print 'Before setting Nonin clock:'
 print current_time.strftime('   Time on Nonin, GMT: %Y-%m-%d %H:%M:%S UDT')
 print current_time.astimezone(tz.tzlocal()).strftime('   Time on Nonin, translated to local timezone: %Y-%m-%d %H:%M:%S%z')
 
-#print 'Setting clock on Nonin to current host time...'
-#nonin.set_current_time()
+print 'Setting clock on Nonin to current host time...'
+nonin.set_current_time()
 
-#current_time = nonin.get_current_time()
-#print 'After setting Nonin clock:'
-#print current_time.strftime('   Time on Nonin, GMT: %Y-%m-%d %H:%M:%S UDT')
-#print current_time.astimezone(tz.tzlocal()).strftime('   Time on Nonin, translated to local timezone: %Y-%m-%d %H:%M:%S%z')
+current_time = nonin.get_current_time()
+print 'After setting Nonin clock:'
+print current_time.strftime('   Time on Nonin, GMT: %Y-%m-%d %H:%M:%S UDT')
+print current_time.astimezone(tz.tzlocal()).strftime('   Time on Nonin, translated to local timezone: %Y-%m-%d %H:%M:%S%z')
 
