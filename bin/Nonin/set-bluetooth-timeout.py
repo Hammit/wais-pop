ff4ecb66130b67e599e1b02dd2c03c872bf50f46
Byron Hammond <byron_hammond@yahoo.com.au>
2016-05-24 10:11:52 +0800

    Split the datetime.py into 2 separate scripts to get and set the datetime and added rubstness to the bluetooth-timeout setting script

--- bin/Nonin/set-bluetooth-timeout.py
+++ bin/Nonin/set-bluetooth-timeout.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2
 
+import sys
 from Nonin import *
-device = Nonin3150.get_device()
-nonin = Nonin3150(device)
 
-# Set time on Nonin from laptop, using GMT timezone
-current_time = nonin.get_current_time()
-print 'Before setting Timeout:'
-print current_time.strftime('   Time on Nonin, GMT: %Y-%m-%d %H:%M:%S UDT')
-print current_time.astimezone(tz.tzlocal()).strftime('   Time on Nonin, translated to local timezone: %Y-%m-%d %H:%M:%S%z')
+ack_problem = True
 
-timeout = 0
-print 'Setting Bluetooth Timeout Period: %d' % timeout
-nonin.set_bluetooth_timeout(timeout)
+while ack_problem:
+    try:
+        # device = Nonin3150.get_device()
+        device = sys.argv[1]
+        nonin = Nonin3150(device)
+
+        timeout = 0
+        print 'Setting Bluetooth Timeout Period: %d' % timeout
+        nonin.set_bluetooth_timeout(timeout)
+
+    except Exception:
+        ack_problem = True
+        time.sleep(1)
+
