c663a503d57f48995ac583c1fc4fabf634f3beb2
Byron <byron_hammond@yahoo.com.au>
2016-05-26 13:18:53 +0800

    Added argparse code to deal with command line arguments

--- src/test/plot.py
+++ src/test/plot.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python2
 
+import argparse
+import os.path
 import matplotlib.pyplot as plt
 import pandas
 import glob
 
-files = glob.glob('/tmp/wristox-sessions.2.csv')
-files.sort()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--csv_file", help="csv file to plot")
+parser.add_argument("--csv_files", nargs='+', help="csv files to combine into a single plot")
+args = parser.parse_args()
+
+
+files = []
+if args.csv_file:
+	files.append(args.csv_file)
+elif args.csv_files:
+	files = args.csv_files
+	files.sort()
+else:
+	print "No files specified"
+
 
 for file in files:
-	df = pandas.read_csv(file, header=None, parse_dates=[0], infer_datetime_format=True)
-	dates = df[0]
-	col_1 = df[1]
-	col_2 = df[2]
-	plt.plot(dates, col_1, 'g-', dates, col_2, 'b-')
-	plt.show()
+	if os.path.getsize(file) > 0:
+		df = pandas.read_csv(file, header=None, parse_dates=[0], infer_datetime_format=True)
+		dates = df[0]
+		col_1 = df[1]
+		col_2 = df[2]
+		plt.plot(dates, col_1, 'g-', dates, col_2, 'b-')
+		plt.show()
