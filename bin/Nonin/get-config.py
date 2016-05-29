fb9e20dc99fc8dbb441ceba96321e1aec021fbdd
Byron <byron_hammond@yahoo.com.au>
2016-05-29 15:15:30 +0800

    Added get-config.py

---
+++ bin/Nonin/get-config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+
+import sys, time
+from Nonin import *
+
+# nonin.enable_logging()
+
+finished = False
+
+while not finished:
+    try:
+        # device = Nonin3150.get_device()
+        device = sys.argv[1]
+        nonin = Nonin3150(device)
+
+        config = nonin.get_config()
+        finished = True
+
+    except Exception:
+        time.sleep(2)
+
+
+import pdb
+pdb.set_trace()
+
+print config
