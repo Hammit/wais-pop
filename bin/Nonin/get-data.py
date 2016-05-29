664798bc124939bcd4d5ea4175c3e7ce791c5df6
Byron Hammond <byron_hammond@yahoo.com.au>
2016-05-29 15:40:02 +0800

    reformatting. tabs to spaces

--- bin/Nonin/get-data.py
+++ bin/Nonin/get-data.py
@@ -7,30 +7,30 @@ finished = False
 
 while not finished:
     try:
-		device = sys.argv[1]
-		nonin = Nonin3150(device)
+        device = sys.argv[1]
+        nonin = Nonin3150(device)
 
-		time.sleep(2)
+        time.sleep(2)
 
-		print 'Attempting to read session data on the device...'
-		sessions = nonin.read_sessions()
+        print 'Attempting to read session data on the device...'
+        sessions = nonin.read_sessions()
 
-		dt = datetime.datetime.now()
-		dt_str = dt.strftime('%Y-%m-%dT%H:%M:%S')
+        dt = datetime.datetime.now()
+        dt_str = dt.strftime('%Y-%m-%dT%H:%M:%S')
 
-		csv_filename = '/tmp/wristox-sessions.%d.%s.csv' % (982146, dt_str)
+        csv_filename = '/tmp/wristox-sessions.%d.%s.csv' % (982146, dt_str)
 
-		print 'Exporting data...'
-		exporter = Exporter(sessions)
-		exporter.export(format='csv', filename=csv_filename)
+        print 'Exporting data...'
+        exporter = Exporter(sessions)
+        exporter.export(format='csv', filename=csv_filename)
 
-		finished = True
+        finished = True
 
     except Exception as e:
-		print e
-		# if nonin:
-		# 	nonin.device.close()
-		time.sleep(2)
+        print e
+        # if nonin:
+        # 	nonin.device.close()
+        time.sleep(2)
 
 
 # import pdb
