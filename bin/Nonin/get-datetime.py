21e0dc9f1adacdc01442cfc224a39f1e3943b478
Byron <byron_hammond@yahoo.com.au>
2016-05-30 14:33:56 +0800

    Minor tweaks to retry logic

--- bin/Nonin/get-datetime.py
+++ bin/Nonin/get-datetime.py
@@ -4,22 +4,18 @@ import sys
 import time
 from Nonin import *
 
-# nonin.enable_logging()
+finished = False
 
-ack_problem = True
-
-while ack_problem:
+while not finished:
     try:
         # device = Nonin3150.get_device()
         device = sys.argv[1]
         nonin = Nonin3150(device)
 
-        # Set time on Nonin from laptop, using GMT timezone
         current_time = nonin.get_current_time()
-        ack_problem = False
+        finished = True
 
     except Exception:
-        ack_problem = True
         time.sleep(1)
 
 print 'Time Information'
